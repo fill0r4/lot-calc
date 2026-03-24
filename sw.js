b251cb7e4067fe5b38b7bd6479612791c371c2d3
fill0r4 <71881575+fill0r4@users.noreply.github.com>
2026-03-24 09:39:05 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lotcalc-v4';
+const CACHE = 'lotcalc-v3';
 const FILES = ['/lot-calc/', '/lot-calc/index.html', '/lot-calc/manifest.json', '/lot-calc/icon-192.png', '/lot-calc/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
